8699a597ab0dda27dbc11fe2893d4523415926fc
Bryan Cairns <bcairns@voidrealms.com>
2017-11-02 15:23:09 -0400

    First commit

---
+++ CS 18/CS 18/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void WriteFile(string Filename)
+        {
+            File.WriteAllText(Filename, "Hello World");
+        }
+
+        static void ReadFile(string Filename)
+        {
+            Console.WriteLine(File.ReadAllText(Filename));
+        }
+
+
+        static void Main(string[] args)
+        {
+            string Filename = @"E:\test\mydoc.txt";
+
+            WriteFile(Filename);
+
+            ReadFile(Filename);
+
+            Console.Read();
+        }
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,10 @@
+Code for video tutorial
+
+Title: C# 18 - Basic File IO
+Published: 9/29/2011 3:38:56 PM
+Link: https://www.youtube.com/watch?v=kGZD_k1M938
+
+Description: Learn the basics of reading and writting files
+
+If you found this helpful, please consider donating: http://www.voidrealms.com
+
